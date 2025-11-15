5f8e3173f584e71c4ec3f4598f570e0f2762e5e9
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-11-15 01:38:03 -0500

    you can change direction

--- ParthenonPyGame.py
+++ ParthenonPyGame.py
@@ -1,13 +1,41 @@
 import pygame
 
 pygame.init()
+Width = 640
+Height = 640
 
-screen = pygame.display.set_mode((640,640))
+screen = pygame.display.set_mode((Width,Height))
 direction = 0
+charachter = pygame.image.load("BlueDino1.png").convert()
+scaled_charachter = pygame.transform.scale(charachter,(100,100))
+
+Midpoint = Height/2
 
 running = True
+pygame.display.Info()
+
+start_x = 50
+start_y = 50
+speed = 0.1
 while running:
+    ##this is while the game is going so infinte loop
+    screen.blit(scaled_charachter, (start_x,Midpoint))
+    print(direction)
+    start_x += speed
+
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                direction = -1
+            elif event.key == pygame.K_DOWN:
+                direction = 1
+        ## if not up or down
+        elif event.type == pygame.KEYUP:
+            if event.key in (pygame.K_UP, pygame.K_DOWN):
+                direction = 0
+    pygame.display.flip()
 pygame.quit
