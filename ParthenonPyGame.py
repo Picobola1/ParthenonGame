d121f918b0d517eee6ad4ffb96dc3bcf92f8e4ab
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-11-15 00:59:15 -0500

    you can close the window

--- ParthenonPyGame.py
+++ ParthenonPyGame.py
@@ -1,1 +1,13 @@
-print("hi")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640,640))
+direction = 0
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit
