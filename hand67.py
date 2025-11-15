5eb4ba55c468f6dbc8b2c16140c029daf77299ed
kiki <74992761+kiki2010@users.noreply.github.com>
2025-11-15 11:19:57 -0500

    now detects Hands | yay

--- hand67.py
+++ hand67.py
@@ -1,9 +1,27 @@
 import cv2 as cv
 import mediapipe as mp
 
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+
 cap = cv.VideoCapture(0)
 
 while True:
-    _, img = cap.read()
+    ret, img = cap.read()
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+    if not ret:
+        break
 
-    cv.imshow('webcam', img)
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)   
+    results = mp_hands.Hands().process(img)
+
+    img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(img, hand_landmarks, connections = mp_hands.HAND_CONNECTIONS)
+
+    cv.imshow('webcam', img)    
+
+cv.destroyAllWindows()
