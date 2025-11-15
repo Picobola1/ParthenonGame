730a71df8d43f3edba2644b3b419724719275b21
kiki2010 <chiaraangelinacatalini@gmail.com>
2025-11-15 01:33:29 -0500

    now faces can be detected --> I need to make it save the values on a variable for the dino jump

--- followhead.py
+++ followhead.py
@@ -1,46 +1,28 @@
 """
+Face Detection
 """
 import cv2 as cv
 import numpy as np
 
 cap = cv.VideoCapture(0)
-pizza = cv.imread('pizza.jpg')
-img = cv.imread('6.jpg')
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
-
-## show the image with edits
-
-while True:
-    _, img = cap.read()
-    
-
-    key = cv.waitKey(1) & 0xFF
-    if key == ord('q'):   # press q to quit
+# Detects face and draws a rectangle
+while True: 
+    ret, img = cap.read()
+    if not ret:
         break
+
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces_react = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
+    for (x, y, w, h) in faces_react:
+        cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
 
-    haar_cascadeFaces = cv.CascadeClassifier('haar_face.xml')
-    #detect a face and return rectangluar coordinates as a list
-    rect_around_face = haar_cascadeFaces.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-
-
-
-
-    for (x,y,w,h) in rect_around_face:
-        cv.rectangle(img, (x,y), (x+w,y + h), (100,100,0), thickness=2)
-
-        resize = cv.resize(pizza, (w,h))
-        img[y:y+h, x:x+w] = resize
     cv.imshow('frame-1', img)
 
-
-
-
-
-
-
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
-cv.waitKey(0)
 cv.destroyAllWindows()
